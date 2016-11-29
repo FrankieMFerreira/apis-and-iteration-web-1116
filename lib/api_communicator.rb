4422542df8b8fdfe8a4dbaf6324f2d9cdc30937e
FrankieMFerreira <FrankieMFerreira@gmail.com>
2016-11-29 15:34:11 -0500

    Done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,31 +3,33 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+ #make the web request
+ all_characters = RestClient.get('http://www.swapi.co/api/people/')
+ character_hash = JSON.parse(all_characters)
+
+   films_hash = []
+
+   character_hash["results"].each do |element|
+     if element["name"].downcase == character
+         element["films"].each do |film|
+           films_hash << JSON.parse(RestClient.get(film.to_s))
+         end
+     end
+   end
+films_hash
 end
 
+
+
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+   films_hash.each.with_index(1) do |movie, index|
+     puts "#{index}. #{movie["title"]}"
+     puts "Director: #{movie["director"]}"
+
+   end
 end
 
 def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+ films_hash = get_character_movies_from_api(character)
+ parse_character_movies(films_hash)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
